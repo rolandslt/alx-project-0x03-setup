ae883a3e54f9cc96cd1de805e9d524ad32335abb
rolandslt <163386313+rolandslt@users.noreply.gituhb.com>
2025-07-27 23:25:15 +0100

    Refactor: move interfaces to interface/index.ts for cleaner structure

---
+++ alx-project-0x03/interface/index.ts
@@ -0,0 +1,8 @@
+export interface ButtonProps {
+  size: 'small' | 'medium' | 'large';
+  shape: 'rounded-sm' | 'rounded-md' | 'rounded-full';
+  children: React.ReactNode;
+}
+export interface LayoutProps {
+  children: React.ReactNode;
+}
